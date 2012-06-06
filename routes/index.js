2a6441e895134f2e0d23e6f49275d6602ed9edfa
Adrian <adicirstei@gmail.com>
2012-06-06 08:54:07 -0400

    dependecy change

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,18 @@
 /*
  * GET home page.
  */
+var couch = require('couch'),
+    c = couch('http://blog:bl0gger@adicirstei.iriscouch.com/blog_cirstei_ro');
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+    c.post({'msg':'new document'}, function (e, info) {
+        if (e){ 
+            throw e;
+        } else{
+            console.log('ok');
+            console.log(info);
+        }
+    });
+
+    res.render('index', { title: 'Express' })
 };
