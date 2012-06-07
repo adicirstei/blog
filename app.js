9906c928ba4ad300cb39cfd6050ca29805ce35f7
Adrian <adicirstei@gmail.com>
2012-06-07 23:38:15 +0300

    sssss

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ var app = module.exports = express.createServer();
 // Configuration
 
 app.configure(function(){
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: "keyb0ard sat" }));
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.bodyParser());
@@ -19,7 +21,11 @@ app.configure(function(){
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
-
+app.dynamicHelpers({
+  session: function(req, res){
+    return req.session;
+  }
+});
 app.configure('development', function(){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
@@ -29,8 +35,8 @@ app.configure('production', function(){
 });
 
 // Routes
+routes.setup(app);
 
-app.get('/', routes.index);
 console.log(settings);
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,14 @@
  */
 //var couch = require('couch'),
 //    c = couch('http://blog:bl0gger@adicirstei.iriscouch.com/blog_cirstei_ro');
-
-exports.index = function(req, res){
+exports.setup = function(app){
+  app.get('/', function(req, res){
   
     res.render('index', { title: 'Express' })
-};
+  });
+  
+  
+  app.get('/login', function(req, res){
+    res.render('login');
+  }
+}
