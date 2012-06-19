93329d06ce0d0dde7174a9c04664ff4ed5b7ba1b
ide user ide_adicirstei <adicirstei@gmail.com>
2012-06-19 11:57:26 +0000

    try to split it again

--- app.js
+++ app.js
@@ -2,101 +2,15 @@ var express = require('express');
 var routes = require('./routes');
 var everyauth = require('everyauth')
   , settings = require('./settings')
-  , Promise = everyauth.Promise;
-
-everyauth.debug = true;
-
-var mongoose = require('mongoose')
-  , Schema = mongoose.Schema
-  , ObjectId = mongoose.SchemaTypes.ObjectId;
-
-var UserSchema = new Schema({})
-  , User;
-var mongooseAuth = require('mongoose-auth');
-
-
-UserSchema.plugin(mongooseAuth, {
-    everymodule: {
-      everyauth: {
-          User: function () {
-            return User;
-          }
-      }
-    }
-/*  , facebook: {
-      everyauth: {
-          myHostname: 'http://www.cirstei.ro'
-        , appId: conf.fb.appId
-        , appSecret: conf.fb.appSecret
-        , redirectPath: '/'
-      }
-    } */
-  , twitter: {
-      everyauth: {
-          myHostname: 'http://www.cirstei.ro'
-        , consumerKey: settings.twitter.conskey
-        , consumerSecret: settings.twitter.secret
-        , redirectPath: '/'
-      }
-    }
-  , password: {
-        loginWith: 'email'
-      , extraParams: {
-            phone: String
-          , name: {
-                first: String
-              , last: String
-            }
-        }
-      , everyauth: {
-            getLoginPath: '/login'
-          , postLoginPath: '/login'
-          , loginView: 'login.jade'
-          , getRegisterPath: '/register'
-          , postRegisterPath: '/register'
-          , registerView: 'register.jade'
-          , loginSuccessRedirect: '/'
-          , registerSuccessRedirect: '/'
-        }
-    }
-/*  , github: {
-      everyauth: {
-          myHostname: 'http://www.cirstei.ro'
-        , appId: conf.github.appId
-        , appSecret: conf.github.appSecret
-        , redirectPath: '/'
-      }
-    }
-  , instagram: {
-      everyauth: {
-          myHostname: 'http://www.cirstei.ro'
-        , appId: conf.instagram.clientId
-        , appSecret: conf.instagram.clientSecret
-        , redirectPath: '/'
-      }
-    } */
-  , google: {
-      everyauth: {
-          myHostname: 'http://www.cirstei.ro'
-        , appId: "224794776836-cp3a2v0elt955h9uqhgmskplhg85ljjm.apps.googleusercontent.com"
-        , appSecret: "rxGFo1mBG_H3DX2ifDFawiMZ"
-        , redirectPath: '/'
-        , scope: 'https://www.google.com/m8/feeds'
-      }
-    }
-});
-
-mongoose.model('User', UserSchema);
-
-mongoose.connect('mongodb://localhost/blog');
-User = mongoose.model('User');
-
+  , auth = require('./auth');
+  
+  
 var app = express.createServer(
     express.bodyParser()
   , express.static(__dirname + "/public")
   , express.cookieParser()
   , express.session({ secret: 'esoognom'})
-  , mongooseAuth.middleware()
+  , auth.middleware()
 );
 app.configure(function(){
 //  app.use(express.cookieParser());
@@ -128,7 +42,7 @@ app.configure('production', function(){
 
 // Routes
 routes.setup(app);
-mongooseAuth.helpExpress(app);
+auth.helpExpress(app);
 
 
 //console.log(settings);
--- auth.js
+++ auth.js
@@ -1,5 +1,7 @@
 var everyauth = require('everyauth')
   , settings = require('./settings')
+
+
   , Promise = everyauth.Promise;
 
 everyauth.debug = true;
@@ -10,84 +12,83 @@ var mongoose = require('mongoose')
 
 var UserSchema = new Schema({})
   , User;
-debugger;
 var mongooseAuth = require('mongoose-auth');
-module.exports = exports = mongooseAuth;
-module.exports.setup = exports.setup = function (){
-    UserSchema.plugin(mongooseAuth, {
-        everymodule: {
-          everyauth: {
-              User: function () {
-                return User;
-              }
-          }
-        }
-    /*  , facebook: {
-          everyauth: {
-              myHostname: 'http://www.cirstei.ro'
-            , appId: conf.fb.appId
-            , appSecret: conf.fb.appSecret
-            , redirectPath: '/'
-          }
-        } */
-      , twitter: {
-          everyauth: {
-              myHostname: 'http://www.cirstei.ro'
-            , consumerKey: settings.twitter.conskey
-            , consumerSecret: settings.twitter.secret
-            , redirectPath: '/'
-          }
-        }
-      , password: {
-            loginWith: 'email'
-          , extraParams: {
-                phone: String
-              , name: {
-                    first: String
-                  , last: String
-                }
-            }
-          , everyauth: {
-                getLoginPath: '/login'
-              , postLoginPath: '/login'
-              , loginView: 'login.jade'
-              , getRegisterPath: '/register'
-              , postRegisterPath: '/register'
-              , registerView: 'register.jade'
-              , loginSuccessRedirect: '/'
-              , registerSuccessRedirect: '/'
-            }
-        }
-    /*  , github: {
-          everyauth: {
-              myHostname: 'http://www.cirstei.ro'
-            , appId: conf.github.appId
-            , appSecret: conf.github.appSecret
-            , redirectPath: '/'
-          }
-        }
-      , instagram: {
-          everyauth: {
-              myHostname: 'http://www.cirstei.ro'
-            , appId: conf.instagram.clientId
-            , appSecret: conf.instagram.clientSecret
-            , redirectPath: '/'
-          }
-        } */
-      , google: {
-          everyauth: {
-              myHostname: 'http://www.cirstei.ro'
-            , appId: "224794776836-cp3a2v0elt955h9uqhgmskplhg85ljjm.apps.googleusercontent.com"
-            , appSecret: "rxGFo1mBG_H3DX2ifDFawiMZ"
-            , redirectPath: '/'
-            , scope: 'https://www.google.com/m8/feeds'
-          }
-        }
-    });
-    
-    mongoose.model('User', UserSchema);
-    
-    mongoose.connect('mongodb://localhost/blog');
-    User = mongoose.model('User');
 
-}
+
+UserSchema.plugin(mongooseAuth, {
+    everymodule: {
+      everyauth: {
+          User: function () {
+            return User;
+          }
+      }
+    }
+/*  , facebook: {
+      everyauth: {
+          myHostname: 'http://www.cirstei.ro'
+        , appId: conf.fb.appId
+        , appSecret: conf.fb.appSecret
+        , redirectPath: '/'
+      }
+    } */
+  , twitter: {
+      everyauth: {
+          myHostname: 'http://www.cirstei.ro'
+        , consumerKey: settings.twitter.conskey
+        , consumerSecret: settings.twitter.secret
+        , redirectPath: '/'
+      }
+    }
+  , password: {
+        loginWith: 'email'
+      , extraParams: {
+            phone: String
+          , name: {
+                first: String
+              , last: String
+            }
+        }
+      , everyauth: {
+            getLoginPath: '/login'
+          , postLoginPath: '/login'
+          , loginView: 'login.jade'
+          , getRegisterPath: '/register'
+          , postRegisterPath: '/register'
+          , registerView: 'register.jade'
+          , loginSuccessRedirect: '/'
+          , registerSuccessRedirect: '/'
+        }
+    }
+/*  , github: {
+      everyauth: {
+          myHostname: 'http://www.cirstei.ro'
+        , appId: conf.github.appId
+        , appSecret: conf.github.appSecret
+        , redirectPath: '/'
+      }
+    }
+  , instagram: {
+      everyauth: {
+          myHostname: 'http://www.cirstei.ro'
+        , appId: conf.instagram.clientId
+        , appSecret: conf.instagram.clientSecret
+        , redirectPath: '/'
+      }
+    } 
+  , google: {
+      everyauth: {
+          myHostname: 'http://www.cirstei.ro'
+        , appId: "224794776836-cp3a2v0elt955h9uqhgmskplhg85ljjm.apps.googleusercontent.com"
+        , appSecret: "rxGFo1mBG_H3DX2ifDFawiMZ"
+        , redirectPath: '/'
+        , scope: 'https://www.google.com/m8/feeds'
+      }
+    } */
+});
+
+mongoose.model('User', UserSchema);
+
+mongoose.connect('mongodb://localhost/blog');
+User = mongoose.model('User');
+module.exports = mongooseAuth;
+
