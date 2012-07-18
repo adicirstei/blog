3cc5cabd6fa3cbd52bf8dab5b72f552f14355ce8
Adrian <adicirstei@gmail.com>
2012-07-18 22:47:05 +0300

    jsformat

--- auth.js
+++ auth.js
@@ -1,22 +1,24 @@
 var models = require('./models');
 var User = models.User;
 
-var hash = function(data){
-	var crypto = require('crypto');
-	var shasum = crypto.createHash('sha1');
-	shasum.update(data);
-	var d = shasum.digest('hex');
-	return d;
-}
+var hash = function(data) {
+		var crypto = require('crypto');
+		var shasum = crypto.createHash('sha1');
+		shasum.update(data);
+		var d = shasum.digest('hex');
+		return d;
+	}
 module.exports = {
-	login: function (user, pass, fn){
-		User.findOne({ username: user, password: hash(pass)}, ['username', 'email'], function (err, u){
-			if (err || !u){
+	login: function(user, pass, fn) {
+		User.findOne({
+			username: user,
+			password: hash(pass)
+		}, ['username', 'email'], function(err, u) {
+			if (err || !u) {
 				fn(null);
-        return;
+				return;
 			}
-      fn(u);
+			fn(u);
 		});
 	}
 };
-
